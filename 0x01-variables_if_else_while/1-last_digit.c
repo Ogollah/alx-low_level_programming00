d8105850d4cb827774d0bd34153e759b57a51d17
Ogollah <kogollah2011@gmail.com>
2022-07-03 09:56:33 -0700

    Fix space

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	if (last_digit > 5)
-		printf("%s %d is %d and is greater than 5\n",str, n, last_digit);
+		printf("%s %d is %d and is greater than 5\n", str, n, last_digit);
 	else if (last_digit == 0)
-		printf("%s %d is %d and is 0\n",str, n, last_digit);
+		printf("%s %d is %d and is 0\n", str, n, last_digit);
 	else if (last_digit < 6 && last_digit != 0)
-		printf("%s %d is %d and is less than 6 and not 0\n",str, n, last_digit);
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, last_digit);
 		
 	return (0);
 }
