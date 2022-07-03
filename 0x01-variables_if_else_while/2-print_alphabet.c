f1ff5b741808f468bbce76a2406121b2e0933e98
Ogollah <kogollah2011@gmail.com>
2022-07-03 10:53:55 -0700

    Fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++){
+	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-        	putchar('\n');
-		return (0);
-	}
+		putchar("\n");
+
+	return (0);
 }
