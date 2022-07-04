cf202229658249af1b71474b94ae257428d4ccd0
Ogollah <kogollah2011@gmail.com>
2022-07-04 12:31:20 -0700

    Com

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,14 @@ int main(void)
 	int num;
 
 	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
 		if (num != '9')
 		{
-			putchar(num);
 			putchar(',');
-		}
-		else if (num == '9' || num != '9')
-		{
-			putchar(num);
 			putchar(' ');
 		}
+	}
+	putchar('\n');
 	return (0);
 }
