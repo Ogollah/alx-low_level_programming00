cf8b8be4abb6131660597d19d41bcd8bbc83f218
Ogollah <kogollah2011@gmail.com>
2022-07-08 05:55:35 -0700

    print alphabet in lower 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet_x10 - prints alphabets in lower cases 10x
+ *
+ * Return: 0.
+ */
+void print_alphabet_x10(void)
+{
+	int lower;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (lower = 'a'; lower <= 'z'; lower++)
+			putchar(lower);
+		putchar('\n');
+	}
+}
